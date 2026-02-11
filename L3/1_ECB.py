8ec30797a80d81d6656170bf7916cd27116093fc
Imperfect <86518966+imperfectoRK@users.noreply.github.com>
2026-02-11 15:34:41 +0530

    Refactor encryption and decryption functions

--- L3/1_ECB.py
+++ L3/1_ECB.py
@@ -11,13 +11,14 @@ def unpad(data):
 
 
 key = b"midsem is coming"
+
 cipher = AES.new(key, AES.MODE_ECB)
 
 
 
 
-
-def encryptblocks(paddedPT):
+# encrytion funtion
+def encryptfun(paddedPT):
     blocks = []
     for i in range(0, len(paddedPT), block_size):
         blocks.append(paddedPT[i:i+block_size])
@@ -28,7 +29,10 @@ def encryptblocks(paddedPT):
 
     return b"".join(cblocks)
 
-def decryptblocks(ciphertext):
+
+
+# decryption function
+def decryptfun(ciphertext):
     blocks = []
     for i in range(0, len(ciphertext), block_size):
         blocks.append(ciphertext[i:i+block_size])
@@ -44,31 +48,21 @@ def decryptblocks(ciphertext):
 
 
 
-print("\n 1st original victim message  ")
+print("\n 1st original message  ")
+senderPT = (b"user=rahul______"b"role=user_______"b"valid=yes_______")
+print("plaintext:", senderPT)
 
-pt_victim = (
-    b"user=rahul______"
-    b"role=user_______"
-    b"valid=yes_______"
-)
-
-print("plaintext:", pt_victim)
-
-ct_victim = encryptblocks(pad(pt_victim))
+senderCT = encryptfun(pad(senderPT))
 
 
 
 
 
-print("\n 2nd  attacker observes another ciphertext  ")
+print("\n 2nd  observed another ciphertext  ")
 
-pt_admin = (
-    b"user=admin______"
-    b"role=admin______"
-    b"valid=yes_______"
-)
-
-ct_admin = encryptblocks(pad(pt_admin))
+adminPT = (b"user=admin______"b"role=admin______"b"valid=yes_______")
+print(adminPT)
+adminCT = encryptfun(pad(adminPT))
 
 
 
@@ -76,25 +70,31 @@ ct_admin = encryptblocks(pad(pt_admin))
 
 
 print("\n 3rd attacker cuts and pastes blocks  ")
+print("replacing role=user with ==> role=admin block")
+# split sender ciphertext
+sender_blocks = []
 
-# split victim ciphertext
-victim_blocks = []
-for i in range(0, len(ct_victim), block_size):
-    victim_blocks.append(ct_victim[i:i+block_size])
+for i in range(0, len(senderCT), block_size):
+    sender_blocks.append(senderCT[i:i+block_size])
+
+
 
 # split admin ciphertext
 admin_blocks = []
-for i in range(0, len(ct_admin), block_size):
-    admin_blocks.append(ct_admin[i:i+block_size])
-
-# replace role=user block with role=admin block
-victim_blocks[1] = admin_blocks[1]
-
-tampered_ciphertext = b"".join(victim_blocks)
+for i in range(0, len(adminCT), block_size):
+    admin_blocks.append(adminCT[i:i+block_size])
 
 
 
-print("\n 4th victim decrypts  ")
 
-tampered_plaintext = decryptblocks(tampered_ciphertext)
-print("tampered plaintext:", tampered_plaintext)
+# replacing role=user with ==> role=admin block
+
+sender_blocks[1] = admin_blocks[1]
+CTdash = b"".join(sender_blocks)
+
+
+
+print("\n 4th reciever decrypts  ")
+
+PTdash = decryptfun(CTdash)
+print("modified plaintext:", PTdash)
