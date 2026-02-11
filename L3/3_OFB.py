2d0483b4387df20d1495a26dcfd4bb89d11b531e
Imperfect <86518966+imperfectoRK@users.noreply.github.com>
2026-02-11 16:42:13 +0530

    Update IV and bit flip logic in OFB encryption
    Modified the initialization vector and updated the bit flip process for encryption.

--- L3/3_OFB.py
+++ L3/3_OFB.py
@@ -2,7 +2,7 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
 key = b"midsem is coming"
-iv  = get_random_bytes(16)
+iv  = b"start  preparing"
 
 def encrypt(pt):
     cipher = AES.new(key, AES.MODE_OFB, iv=iv)
@@ -22,6 +22,8 @@ print(ct[:16])
 print(ct[16:32])
 
 
+
+#bit flip
 print("\n 1st original message  ")
 
 pt = b"user=rahul;access=read;token=100;"
@@ -36,14 +38,17 @@ ct_mod = bytearray(ct)
 
 # change quota=100 -> quota=900
 idx = pt.index(b"100")
+original = ord('1')      
+desired  = ord('9')      
+XOR_value = ord('1') ^ ord('9')
+ct_mod[idx] ^= XOR_value
 
-# '1' (0x31) -> '9' (0x39) => xor 0x08
-ct_mod[idx] ^= 0x08
 
-tampered_ct = bytes(ct_mod)
+
+modifiedCT = bytes(ct_mod)
 
 
 print("\n 3rd victim decrypts  ")
 
-tampered_pt = decrypt(tampered_ct)
-print("tampered plaintext:", tampered_pt)
+modifiedPT = decrypt(modifiedCT)
+print("modified plaintext:", modifiedPT)
